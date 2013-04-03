eab10281d3a721a4f269ef7cf0ea4243c121830d
Alexander Bakst <abakst@cs.ucsd.edu>
2013-04-03 11:50:45 -0700

    Break up mg_connection and mg_auth

---
+++ mg_auth.h
@@ -0,0 +1,18 @@
+#include "mongoose.h"
+#include "mongoose-policy.h"
+
+#include "mg_util.h"
+
+int
+REF((V != 0) => ? AUTHORIZED([CONN([conn])]))
+check_authorization(struct mg_connection   * OK OK_CONN conn,
+                    const NULLTERMSTR char * STRINGPTR  path)
+  OKEXTERN;
+
+int REF((V != 0) => OK_PUT(conn))
+is_authorized_for_put(struct mg_connection * OK OK_CONN conn)
+  OKEXTERN;
+
+void
+send_authorization_request(struct mg_connection * OK M conn)
+  OKEXTERN;
